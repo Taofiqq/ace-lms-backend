120fc474e4d1c9d62100dd78dff4fe44cdd8daa1
Taofiqq <taofiqaiyelabegan45@gmail.com>
2025-05-20 12:06:35 +0100

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   const config = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
 
